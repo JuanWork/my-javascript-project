ca5a448887451ae533f67b00a712e156e4d9bcca
JuanWork <jcespanhol@gmail.com>
2025-10-05 17:07:17 -0300

    adding css, js and html

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+console.log("VS Code + JavaScript = Awesome!");
+
+let myAge = 55;
+let myName = "Juan Carlos";
+
+function showMessage() {
+    console.log("Hello " + myName + ", you are " + myAge + " years old.");
+}
+
+showMessage();  
+
+function calculate() {
+    let result = myAge * 2;
+    console.log( `Valdanusia your age: ${result}`);
+}
+
+calculate();
+    
